5127888df82ff46972c8be58be16abd5e1f05ef1
arekalov <artyom.rekalov@gmail.com>
2024-03-15 18:11:18 +0300

    edit login viewModel
    change encryptedSharedPreferences type to SharedPreferences

--- app/src/main/java/com/example/passkeeper/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/example/passkeeper/viewmodels/LoginViewModel.kt
@@ -1,11 +1,12 @@
 package com.example.passkeeper.viewmodels
 
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModel
 import androidx.security.crypto.EncryptedSharedPreferences
 import com.example.data.security.RepositorySecurity
 import com.google.crypto.tink.Aead
 
-class LoginViewModel(private val encryptedSharedPreferences: EncryptedSharedPreferences, private val aead: Aead) :
+class LoginViewModel(private val encryptedSharedPreferences: SharedPreferences, private val aead: Aead) :
     ViewModel() {
     val securityRepo = RepositorySecurity(aead)
     fun checkPin(pin: String): Boolean {
