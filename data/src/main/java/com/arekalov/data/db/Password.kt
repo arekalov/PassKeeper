6c54860e3fadb4ea09ddadfdbec77c26e77096fe
arekalov <artyom.rekalov@gmail.com>
2024-03-17 12:01:21 +0300

    edit password_card.xml
    add field login
    add Password.kt entity

---
+++ data/src/main/java/com/arekalov/data/db/Password.kt
@@ -0,0 +1,14 @@
+package com.arekalov.data.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "password_table")
+data class Password(
+    @PrimaryKey @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "url") val url: String,
+    @ColumnInfo(name = "login") val login: String,
+    @ColumnInfo(name = "password") val password: String,
+    @ColumnInfo(name = "favicon") val favicon: String
+)
