97ddf88662d210022c002fc3cf6dada7a43143a5
arekalov <artyom.rekalov@gmail.com>
2024-03-17 22:49:53 +0300

    add functionality fro settings btn

--- app/src/main/java/com/arekalov/passkeeper/fragments/SettingsFragment.kt
+++ app/src/main/java/com/arekalov/passkeeper/fragments/SettingsFragment.kt
@@ -5,18 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.arekalov.passkeeper.MainActivity
 import com.arekalov.passkeeper.R
+import com.arekalov.passkeeper.databinding.FragmentSettingsBinding
+import com.arekalov.passkeeper.viewmodels.LoginViewModel
 
 class SettingsFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
+    private lateinit var loginViewModel: LoginViewModel
+    private lateinit var binding: FragmentSettingsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+       binding = FragmentSettingsBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loginViewModel = (activity as MainActivity).loginViewModel
     }
 }
